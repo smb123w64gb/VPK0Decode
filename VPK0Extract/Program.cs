afd8b300bd038357f6322f112f9e5879b20a93e8
smb123w64gb <smb123w64gb@gmail.com>
2019-08-26 15:12:55 -0700

    Add project files.

---
+++ VPK0Extract/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace VPK0Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VPK0 Decrypter");
+            BinaryReader2 reader = new BinaryReader2(File.Open(args[0], FileMode.Open));
+            BinaryWriter writer = new BinaryWriter(File.Open(args[0] + ".dec0", FileMode.Create));
+
+        }
+    }
+}
---
+++ VPK0Extract/VPK0.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace VPK0Extract
+{
+    class VPK
+    {
+        private UInt32 header(byte data)
+        {
+            
+        }
+        public BinaryWriter Decode(BinaryReader2 data,bool header=false)
+        {
+
+            return data;
+            if (!header)
+            {
+
+            }
+        }
+    }
+}
